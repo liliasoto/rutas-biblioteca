9d7c8800b3b4fe610998a7dcef039df8f472ab3e
liliasoto <0.lilia.soto.0@gmail.com>
2023-12-08 11:55:18 -0600

    ADDED comentarios

--- src/paginas/Notfound.js
+++ src/paginas/Notfound.js
@@ -1,3 +1,10 @@
+/*
+--EQUIPO
+- Lilia Soto Llamas 20460040
+- Max Yahir Rodríguez González 20460278
+- Ramses Patiño Cortés 20460181
+*/
+
 import React from "react";
 
 export default function NotFound(){
